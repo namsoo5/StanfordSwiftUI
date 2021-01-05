df290061ecd085f99f3e2b19f05b02095344c2b1
김남수 <are34@naver.com>
2021-01-05 18:47:46 +0900

    UPDATE: Chap1 SwiftUI개념
    - 기본문법

---
+++ StanfordSwiftUI/StanfordSwiftUI/StanfordSwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  StanfordSwiftUIApp.swift
+//  StanfordSwiftUI
+//
+//  Created by 남수김 on 2021/01/05.
+//
+
+import SwiftUI
+
+@main
+struct StanfordSwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}

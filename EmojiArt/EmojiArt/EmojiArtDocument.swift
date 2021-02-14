4a10e9eebc1579e9b5531ad40341892e8aa6b566
김남수 <are34@naver.com>
2021-02-14 15:01:52 +0900

    CREATE: Chap7 EmojiArt 프로젝트생성

---
+++ EmojiArt/EmojiArt/EmojiArtDocument.swift
@@ -0,0 +1,13 @@
+//
+//  EmojiArtDocument.swift
+//  EmojiArt
+//
+//  Created by 남수김 on 2021/02/14.
+//
+
+import SwiftUI
+
+// ViewModel
+class EmojiArtDocument: ObservableObject {
+    
+}
---
+++ EmojiArt/EmojiArt/EmojiArtDocumentView.swift
@@ -0,0 +1,17 @@
+//
+//  ContentView.swift
+//  EmojiArt
+//
+//  Created by 남수김 on 2021/02/14.
+//
+
+import SwiftUI
+
+struct EmojiArtDocumentView: View {
+    @ObservedObject var document: EmojiArtDocument
+    
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}

1a826016369fdbf0d5c4954f614ed1e358d21865
김남수 <are34@naver.com>
2021-02-14 19:32:10 +0900

    UPDATE: Chap7 적용
    이모지, 이미지 drag and drop

---
+++ EmojiArt/EmojiArt/EmojiArt.swift
@@ -0,0 +1,36 @@
+//
+//  EmojiArt.swift
+//  EmojiArt
+//
+//  Created by 남수김 on 2021/02/14.
+//
+
+import Foundation
+
+// Model
+struct EmojiArt {
+    var backgroundURL: URL?
+    var emojis = [Emoji]()
+    var uniqueEmojiId = 0
+    
+    struct Emoji: Identifiable {
+        let text: String
+        var x: Int // offset from center
+        var y: Int // offset from center
+        var size: Int
+        let id: Int
+        
+        fileprivate init(text: String, x: Int, y: Int, size: Int, id: Int) {
+            self.text = text
+            self.x = x
+            self.y = y
+            self.size = size
+            self.id = id
+        }
+    }
+    
+    mutating func addEmoji(_ text: String, x: Int, y: Int, size: Int) {
+        uniqueEmojiId += 1
+        emojis.append(Emoji(text: text, x: x, y: y, size: size, id: uniqueEmojiId))
+    }
+}
--- EmojiArt/EmojiArt/EmojiArtDocumentView.swift
+++ EmojiArt/EmojiArt/EmojiArtDocumentView.swift
@@ -9,9 +9,65 @@ import SwiftUI
 
 struct EmojiArtDocumentView: View {
     @ObservedObject var document: EmojiArtDocument
+    private let defaultEmojiSize: CGFloat = 40
     
     var body: some View {
-        Text("Hello, world!")
-            .padding()
+        VStack {
+            ScrollView(.horizontal) {
+                HStack {
+                    ForEach(EmojiArtDocument.palette.map { String($0) }, id: \.self) { emoji in
+                        Text(emoji)
+                            .font(.system(size: defaultEmojiSize))
+                            .onDrag { return NSItemProvider(object: emoji as NSString) }
+                    }
+                }
+            }
+            .padding(.horizontal)
+            GeometryReader { geometry in
+                ZStack {
+                    Color.white
+                        .overlay(
+                            Group {
+                                if self.document.backgroundImage != nil {
+                                    Image(uiImage: self.document.backgroundImage!)
+                                }
+                            }
+                        )
+                        .edgesIgnoringSafeArea([.horizontal, .bottom])
+                        .onDrop(of: [.image, .text], isTargeted: nil) { providers, location in
+                            // location: drop위치
+                            var location = geometry.convert(location, from: .global)
+                            location = CGPoint(x: location.x - geometry.size.width/2,
+                                               y: location.y - geometry.size.height/2)
+                            return self.drop(providers: providers, at: location)
+                        }
+                }
+                ForEach(self.document.emojis) { emoji in
+                    Text(emoji.text)
+                        .font(self.font(for: emoji))
+                        .position(self.position(for: emoji, in: geometry.size))
+                }
+            }
+        }
+    }
+    
+    private func font(for emoji: EmojiArt.Emoji) -> Font {
+        Font.system(size: emoji.fontSize)
+    }
+    
+    private func position(for emoji: EmojiArt.Emoji, in size: CGSize) -> CGPoint {
+        CGPoint(x: emoji.location.x + size.width/2, y: emoji.location.y + size.height/2)
+    }
+    
+    private func drop(providers: [NSItemProvider], at location: CGPoint) -> Bool {
+        var found = providers.loadFirstObject(ofType: URL.self) { url in
+            self.document.setBackgroundURL(url)
+        }
+        if !found {
+            found = providers.loadObjects(ofType: String.self) { string in
+                self.document.addEmoji(string, at: location, size: self.defaultEmojiSize)
+            }
+        }
+        return found
     }
 }
